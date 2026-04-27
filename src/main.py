519673ece7ed66bc3876da6b26a49a2f243d2d24
RagingCassowary <jakeob@live.co.uk>
2026-04-27 14:20:57 +0100

    14:18 27.04.26: created TextType and TextNode classes, added .gitignore, removed temp /public files, added unit tests for TextType and TextNode

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    dummy = TextNode("Dummy anchor", "link", "https://wwww.fakelink.abc")
+    return dummy
+
+print(main())
+
---
+++ src/test_textnode.py
@@ -0,0 +1,31 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node2 = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        self.assertNotEqual(node, node2)
+    
+    def test_no_url(self):
+        node = TextNode("This is a link", TextType.LINK)
+        self.assertEqual(node.url, None)
+
+    def test_print(self):
+        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        self.assertEqual(node.__repr__(), "TextNode(This is a text node, text, None)")
+    
+    def test_enum(self):
+        node = TextNode("This is an image node", "image")
+        self.assertEqual(node.text_type, TextType.IMAGE)
+
+
+if __name__ == "__main__":
+    unittest.main()
